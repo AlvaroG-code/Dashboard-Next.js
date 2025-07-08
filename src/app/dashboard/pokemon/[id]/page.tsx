38e3c0381d5b1bb0c62a4c03c4d39a751bbbebaa
Alvaro Garcia <alvarogprod@gmail.com>
2025-07-08 14:18:10 +0200

    ignorando

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,3 +1,5 @@
+// app/dashboard/pokemon/[id]/page.tsx
+
 import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
@@ -36,17 +38,17 @@ const getPokemon = async (id: string): Promise<Pokemons> => {
     }
 };
 
-interface PokemonPageProps {
-    params: { id: string } | Promise<{ id: string }>;
-}
-
-export default async function PokemonPage(props: PokemonPageProps) {
-    const params = await props.params;
+// @ts-ignore
+export default async function PokemonPage({
+    params,
+}: {
+    params: { id: string };
+}) {
     const pokemon = await getPokemon(params.id);
 
     return (
         <div className="flex mt-5 flex-col items-center text-slate-800">
-            <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border shadow-lg p-3">
+            <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
                 <div className="mt-2 mb-8 w-full">
                     <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
                         #{pokemon.id} {pokemon.name}
@@ -76,7 +78,7 @@ export default async function PokemonPage(props: PokemonPageProps) {
                     </div>
                 </div>
                 <div className="grid grid-cols-2 gap-4 px-2 w-full">
-                    <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+                    <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
                         <p className="text-sm text-gray-600">Types</p>
                         <div className="text-base font-medium text-navy-700 flex">
                             {pokemon.types.map((type) => (
@@ -87,14 +89,14 @@ export default async function PokemonPage(props: PokemonPageProps) {
                         </div>
                     </div>
 
-                    <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+                    <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
                         <p className="text-sm text-gray-600">Peso</p>
                         <span className="text-base font-medium text-navy-700 flex">
                             {pokemon.weight}
                         </span>
                     </div>
 
-                    <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+                    <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
                         <p className="text-sm text-gray-600">Regular Sprites</p>
                         <div className="flex justify-center">
                             <Image
@@ -113,7 +115,7 @@ export default async function PokemonPage(props: PokemonPageProps) {
                         </div>
                     </div>
 
-                    <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+                    <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
                         <p className="text-sm text-gray-600">Shiny Sprites</p>
                         <div className="flex justify-center">
                             <Image
